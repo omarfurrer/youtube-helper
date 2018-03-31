79234db7d58ecd003c7602b8ffad6d70f2168ffb
Omar Furrer <omar.furrer@gmail.com>
2018-03-31 13:53:16 +0200

    Home page refactoring

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,17 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
 
-Route::get('/', function () {
-    $todos = [
-       
-    ];
-    $todos = [
-        'Get Shit 1',
-        'Get Shit 2',
-        'Get Shit 3',
-        'Get Shit 4',
-        'Get Shit 5'
-    ];
-    return view('home',compact('todos'));
-});
+Route::get('/', 'PagesController@home');
 
 Auth::routes();
 
