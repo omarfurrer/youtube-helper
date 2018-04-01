e640d1501c37f2b3cfe8e1a0c51e9073cd9c583b
Ahmed Al-Gallad <ahmed.algalladd@gmail.com>
2018-04-01 17:56:53 +0200

    Added form validation and minor content changes

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -11,10 +11,10 @@
                             <div class="card-body">
 	         	                 <div class="form-group">
 	         	                 	<img class="img-responsive" src="https://i.imgur.com/fJc8aAG.png">	         	          
-                              <h2>Download youtube videos with ease!</h2>
+                              <h2 id="card-header">Download youtube videos with ease!</h2>
 
          <form action="{{ url('/youtube/link/info') }}" method="GET">
-         <input name="url" type="text" placeholder="Enter your video url here..." class="form-control">
+         <input name="url" type="url" required pattern=".*\.youtube\..*" placeholder="Enter your video or playlist url here..." class="form-control">
               <button class="btn btn-outline-danger btn-lg" type="submit" id="submit">Get Info</button>
                                             </form>
 
