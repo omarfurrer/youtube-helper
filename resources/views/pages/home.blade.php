054a7a8416b89e3db5a2c1e72bb125bf310fce77
Ahmed Al-Gallad <ahmed.algalladd@gmail.com>
2018-04-01 15:07:04 +0200

    Added Homepage front end and custom.css file

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,10 +1,28 @@
 @extends('layouts.main')
 
+
 @section('content')
 
-<form action="{{ url('/youtube/link/info') }}" method="GET">
-    <input name="url" type="text">
-    <button type="submit">Get Info</button>
-</form>
+<!--Search Box-->
+<div class="container text-center">
+       <div class="row">
+	         <div class="col-sm-12">
+                    <div class="card bg-light">
+                            <div class="card-body">
+	         	                 <div class="form-group">
+	         	                 	<img class="img-responsive" src="https://i.imgur.com/fJc8aAG.png">	         	          
+                              <h2>Download youtube videos with ease!</h2>
 
+         <form action="{{ url('/youtube/link/info') }}" method="GET">
+         <input name="url" type="text" placeholder="Enter your video url here..." class="form-control">
+              <button class="btn btn-outline-danger btn-lg" type="submit" id="submit">Get Info</button>
+                                            </form>
+
+                              </div>
+                        </div>
+                  </div>
+            </div>
+      </div>
+</div>
+<!--End of Search Box-->
 @endsection
